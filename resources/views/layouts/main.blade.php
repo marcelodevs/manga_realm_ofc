989d13cc449b0ac2132c40c1c8239f7f310cf118
LuckMG <128225332+marcelodevs@users.noreply.github.com>
2024-06-12 16:04:39 -0300

    feat: adicionado rodapé no site.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,6 +21,19 @@
   </main>
 
   @yield('others')
+  
+  <div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="/create" class="nav-link px-2 text-muted">Criar</a></li>
+        <li class="nav-item"><a href="/authors" class="nav-link px-2 text-muted">Autores</a></li>
+        <li class="nav-item"><a href="/help" class="nav-link px-2 text-muted">Ajuda</a></li>
+        <li class="nav-item"><a href="/about" class="nav-link px-2 text-muted">Sobre nós</a></li>
+      </ul>
+      <p class="text-center text-muted">&copy; 2024 -  MangáRealm</p>
+    </footer>
+  </div>
 
   <!-- BOOTSTRAP -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
